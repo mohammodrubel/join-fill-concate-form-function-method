34e666593f799d3fb542e447270d9e66259e603c
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-08 16:22:45 +0600

    Add files via upload

---
+++ join-fill-concate-form-function-method/function.js
@@ -0,0 +1,18 @@
+
+var arr = [2,3,4,5,6,7]
+var array = [1,2,3,4,5]
+
+// join method 
+var result = arr.join(' | ')
+console.log(result)
+
+// fill mthod 
+// var result2 = arr.fill(0) //arr.fill(true)
+console.log(result2)
+// concate mehtod 
+var result3 = arr.concat(array)
+console.log(result3)
+
+var result4 = Array.from(arr)
+console.log(result4)
+
